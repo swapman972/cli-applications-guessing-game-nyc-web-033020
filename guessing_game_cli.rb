96a3d8e63cd7962764e49ca2eecabd6f51df1572
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-26 20:50:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,5 +2,14 @@
 
 
 def run_guessing_game
-  
+  num = rand(6) + 1
+  puts "take a guess!: "
+  guess = gets.chomp
+  if guess == num
+    puts "You guessed the correct number!"
+  elsif guess == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed " + num + "."
+  end
 end
